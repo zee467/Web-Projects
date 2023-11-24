29c2c0abb050d9f8283ff5ecf87171014b24d284
zee467 <wura.mahmud@gmail.com>
2023-11-24 18:36:50 +0100

    Create Routes With Templates

--- PollingApp/core/__init__.py
+++ PollingApp/core/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from core.auth import auth_bp
 from core.main import main_bp
+import os
+
+from .extensions import db
 
 # application factory
 def create_app():
     app = Flask(__name__)  # create a flask app instance
+    app.secret_key = os.getenv("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
+
+    db.init_app(app)
 
     # register blueprints
     app.register_blueprint(main_bp)
